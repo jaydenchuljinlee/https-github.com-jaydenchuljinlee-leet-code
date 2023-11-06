5ff61c95a52701f8bebb04c6e1da362027d1aa58
Chuljin Lee <34643599+jaydenchuljinlee@users.noreply.github.com>
2023-11-06 12:01:14 +0900

    Time: 0 ms (100.00%), Space: 40.6 MB (30.34%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -22,16 +22,24 @@ class Solution {
     }
     
     ListNode findAndRemove(ListNode head) {
+        // the head is to go back again when the head reach the end point
+        // the flag become false when the head reach the end point
+        // the nn is the point of node to remove. it decrease by one to find the point
+        // what the nn is 1 means to reach the point.
         if (head.next == null) {
             flag = true;
+            
+            // the given n exist the end point
             if (nn == 1) head = null;
 
             nn--;
             return head;
         }
         
+        // if the head isn't the end, it process the recursive logic
         head.next = findAndRemove(head.next);
         
+        // when the given n exist somewhere between the start point and the end point
         if (head.next != null && flag && nn == 1) {
             head = head.next;
         }
